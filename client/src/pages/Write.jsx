c70d76ceee39458be0f62ca8ccda888890ae8845
alaminsheikh01 <alaminsheikh9746@gmail.com>
2022-09-30 20:40:57 +0600

    added initial react-quill rich text editor

--- client/src/pages/Write.jsx
+++ client/src/pages/Write.jsx
@@ -1,9 +1,19 @@
 import React from "react";
+import { useState } from "react";
+import ReactQuil from "react-quill";
+import "react-quill/dist/quill.snow.css";
 
 const Write = () => {
+  const [value, setValue] = useState("");
+  console.log(value);
   return (
     <div className="add">
-      <div className="content">Content</div>
+      <div className="content">
+        <input type="text" placeholder="Title" />
+        <div className="editorContainer">
+          <ReactQuil theme="snow" value={value} onChange={setValue} />
+        </div>
+      </div>
       <div className="menu">
         <div className="item">i1</div>
         <div className="item">i2</div>
